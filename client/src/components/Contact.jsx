a50a2a8767fbed4c3910f33143c82688a81905ee
shree016 <shrilaxmi016@gmail.com>
2025-05-18 12:37:49 +0530

    first update

---
+++ client/src/components/Contact.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import "../styles/contact.css";
+
+const Contact = () => {
+  const [formDetails, setFormDetails] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const inputChange = (e) => {
+    const { name, value } = e.target;
+    console.log("Formspree ID:", process.env.REACT_APP_FORMSPREE_ID);
+    return setFormDetails({
+      ...formDetails,
+      [name]: value,
+    });
+  };
+
+  return (
+    <section
+      className="register-section flex-center"
+      id="contact"
+    >
+      <div className="contact-container flex-center contact">
+        <h2 className="form-heading">Contact Us</h2>
+        <form
+          method="POST"
+          // action={`https://formspree.io/f/${process.env.REACT_APP_FORMSPREE_ID}`}
+          action={`https://formspree.io/f/mwpoybrq`}
+          className="register-form "
+        >
+          <input
+            type="text"
+            name="name"
+            className="form-input"
+            placeholder="Enter your name"
+            value={formDetails.name}
+            onChange={inputChange}
+          />
+          <input
+            type="email"
+            name="email"
+            className="form-input"
+            placeholder="Enter your email"
+            value={formDetails.email}
+            onChange={inputChange}
+          />
+          <textarea
+            type="text"
+            name="message"
+            className="form-input"
+            placeholder="Enter your message"
+            value={formDetails.message}
+            onChange={inputChange}
+            rows="8"
+            cols="12"
+          ></textarea>
+
+          <button
+            type="submit"
+            className="btn form-btn"
+          >
+            send
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
