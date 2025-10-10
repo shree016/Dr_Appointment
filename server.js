7350266220cd29942453684c814c0df60fbdbfbe
shree016 <shrilaxmi016@gmail.com>
2025-10-10 17:19:51 +0530

    deployment railway backend

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`Server running on port ${port}`);
 });
 
